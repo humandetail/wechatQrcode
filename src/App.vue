1aa5900a177b27ab74801f68ce4caafb6e90216f
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-27 13:54:43 +0800

    修改大小写错误问题

--- src/App.vue
+++ src/App.vue
@@ -39,12 +39,12 @@ export default {
       } = res.data;
 
       wx.config({
-        debug: true,
-        appId: appid,
-        timestamp,
-        noncestr,
-        signature,
-        jsApiList: ['scanQRCode']
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: appid, // 必填，公众号的唯一标识
+        timestamp: timestamp, // 必填，生成签名的时间戳
+        nonceStr: noncestr, // 必填，生成签名的随机串
+        signature: signature,// 必填，签名
+        jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表
       });
 
       wx.ready(() => {
