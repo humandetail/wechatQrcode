0c6a5339a1d51640be950ca723b9b094f6a96f8e
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-28 10:28:17 +0800

    修复

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div class="page">
     <h1>test</h1>
@@ -8,9 +7,7 @@
 
 <script>
 const wx = require('weixin-js-sdk');
-
 import axios from 'axios';
-
 export default {
   name: 'App',
   data() {
@@ -23,23 +20,18 @@ export default {
       let res = await axios.post('http://wechat-test.humandetail.com/getSignature', {
         url: location.href.split('#')[0]
       });
-
       res = res.data;
-
-      alert(JSON.stringify(res));
-
+      // alert(JSON.stringify(res));
       // if (res === 'error') {
       //   alert('接口调用失败。');
       //   return false;
       // }
-
       const {
         signature,
         noncestr,
         appid,
         timestamp
       } = res.data;
-
       const config = {
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: appid, // 必填，公众号的唯一标识
@@ -48,14 +40,11 @@ export default {
         signature: signature,// 必填，签名
         jsApiList: ['scanQRCode'] // 必填，需要使用的JS接口列表
       }
-
-      alert(JSON.stringify(config));
-
+      // alert(JSON.stringify(config));
       wx.config(config);
-
       wx.ready(() => {
         wx.scanQRCode({
-          needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+          needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {
             var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
@@ -63,7 +52,6 @@ export default {
           }
         });
       });
-
       wx.error((err) => {
         console.log(err)
       })
