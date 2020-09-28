d3fa71901d625b366565a63453874937e290de1e
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-28 10:35:27 +0800

    rebuild test

--- src/App.vue
+++ src/App.vue
@@ -46,13 +46,14 @@ export default {
       }
       // alert(JSON.stringify(config));
       wx.config(config);
+      const _this = this;
       wx.ready(() => {
         wx.scanQRCode({
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
           success: function (res) {
             var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-            this.result = result
+            _this.result = result
             // alert(result);
           }
         });
