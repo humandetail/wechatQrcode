770435abb64ee38231814c48a40282ec39f8be10
赖汉彬 <humandetail@laihanbindeMacBook-Pro.local>
2020-09-28 11:15:26 +0800

    处理结果

--- src/App.vue
+++ src/App.vue
@@ -63,16 +63,22 @@ export default {
         wx.scanQRCode({
           needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
           scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-          success: function (res) {
-            var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-            _this.result = result
-            // alert(result);
-          }
+          success: _this.onScanSuccess
         });
       });
       wx.error((err) => {
         console.log(err)
       })
+    },
+
+    onScanSuccess (res) {
+      let result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+      this.result = result;
+
+      setTimeout(() => {
+        window.location.href = `${result}${result.indexOf('?') > -1 ? '?' : '&'}from=${window.location.href}&action=publicScan`;
+      }, 2000)
+      // alert(result);
     }
   },
   mounted () {
